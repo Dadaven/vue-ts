2e2ea39cdf47c8170afa499eca959cdaa51cf2a2
Dadaven <1260040667@qq.com>
2018-11-12 15:02:29 +0800

    vue+ts

---
+++ vue-ts/README.md
@@ -0,0 +1,29 @@
+# vue-ts
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+参考网址：https://juejin.im/post/5ba75b355188255c5e66e4d3
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue-ts/src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+<el-container>
+  <el-header>
+    <div>
+    <el-button @click="show2 = !show2">About Me</el-button>
+    <div style="display: flex; margin-top: 20px; height: 100px;">
+      <transition name="el-zoom-in-center">
+        <div v-show="show2" class="transition-box">.el-zoom-in-center</div>
+      </transition>
+      <transition name="el-zoom-in-top">
+        <div v-show="show2" class="transition-box">.el-zoom-in-top</div>
+      </transition>
+      <transition name="el-zoom-in-bottom">
+        <div v-show="show2" class="transition-box">.el-zoom-in-bottom</div>
+      </transition>
+    </div>
+  </div>
+  </el-header>
+  <el-main>
+    <img src="563089882005204171.jpg">
+  </el-main>
+  <el-footer></el-footer>
+</el-container>
+  
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    
+  },
+})
+export default class Home extends Vue {
+   show2:boolean=false;
+}
+</script>
+
+<style>
+  .transition-box {
+    margin-bottom: 10px;
+    width: 200px;
+    height: 100px;
+    border-radius: 4px;
+    background-color: #409EFF;
+    text-align: center;
+    color: #fff;
+    padding: 40px 20px;
+    box-sizing: border-box;
+    margin-right: 20px;
+  }
+</style>
